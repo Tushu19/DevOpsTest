9241273b704d98d172ba8d90693c23e88b33362e
Tushar Gahlot <gahlottushar19@gmail.com>
2023-06-01 07:43:21 +0530

    Update DevopsApplicationTests.java

--- src/test/java/com/example/devops/DevopsApplicationTests.java
+++ src/test/java/com/example/devops/DevopsApplicationTests.java
@@ -8,8 +8,8 @@ class DevopsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		String text = null;
-        int length = text.length();
+		// String text = null;
+        // int length = text.length();
 		// assertEquals(2 + 2, 4, "The sum of 2 and 2 should be 4");
 	}
 
